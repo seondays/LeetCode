de0b5aaa019b6a0abf32a7dcabbd547abbf4d9ec
eunseon <110711591+seondays@users.noreply.github.com>
2024-01-05 13:42:16 +0900

    Time: 110 ms (94.20%), Space: 20 MB (5.78%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,11 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        holder = {}
+        result = 0
         for number in nums:
-            if number in holder:
-                holder[number] += 1
-            else:
-                holder[number] = 1
-        for k,v in holder.items():
-            if v == 1:
-                return k
+            result ^= number
+        return result
