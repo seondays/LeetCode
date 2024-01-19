dc695f4673d46b6b36984a5f227302fb1b54afdb
eunseon <110711591+seondays@users.noreply.github.com>
2024-01-19 22:07:02 +0900

    Time: 25 ms (99.22%), Space: 16.5 MB (52.72%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
@@ -1,6 +1,10 @@
 class Solution:
+    def dsf(self, n):
+        tmp = n[0]
+        if len(n) == 1:
+            return ord(n) - ord('A') + 1
+        return self.dsf(n[1:]) + (26**(len(n)-1) * (ord(tmp) - ord('A') + 1))
+        
     def titleToNumber(self, columnTitle: str) -> int:
-        result = 0
-        for digit in columnTitle:
-            result = result * 26 + ord(digit) - 64
-        return result
+        return self.dsf(columnTitle)
+        
